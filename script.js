912c5fc5430735e4655c616d6dcd77c3ae151db5
Paul Fears <paulrfears@gmail.com>
2020-04-24 21:59:40 +0000

    Rename Class from Node to Graph

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 
+Graph = function(canvasid, fps, mouse_move, editable, buildable){
+      Graph.canvas = document.getElementById(canvasid);
+      Graph.ctx = Graph.canvas.getContext('2d');
+      Graph.objs = {};
+      Graph.edges = [];
+      if(fps == null){
+        fps = 60;
+      }
+      if(mouse_move == null||true){
+        Graph.check_mouse();
+      }
+      if(editable == null||true){
+        Graph.activate_editing(fps);
+      }
+      if(buildable == null||true){
+        Graph.activate_building();
+      }
+      return Graph;
 
+}
 
-function node(x=false, y=false, r=false, text=""){
+Graph.node = function(x=false, y=false, r=false, text=""){
 
     this.children = [];
     this.arrows = {};
@@ -12,22 +31,22 @@ function node(x=false, y=false, r=false, text=""){
     this.func = null;
     this.args = [];
     let date = new Date();
-    this.id = date.getTime()+Object.keys(node.objs).length;
+    this.id = date.getTime()+Object.keys(Graph.objs).length;
     
     this.delete = function(){
-      for(let i = node.edges.length-1; i>-1; i--){
-        edge = node.edges[i];
+      for(let i = Graph.edges.length-1; i>-1; i--){
+        edge = Graph.edges[i];
         if(edge.start === this.id || edge.end === this.id){
-          node.edges.splice(i,1);
+          Graph.edges.splice(i,1);
           
         }
       }
       
-      node.active = null;
+      Graph.active = null;
       let temp_id = this.id;
-      delete node.objs[this.id];
+      delete Graph.objs[this.id];
 
-      for(n of Object.values(node.objs)){
+      for(n of Object.values(Graph.objs)){
 
         position = n.children.indexOf(temp_id);
         if(position !== -1){
@@ -60,11 +79,11 @@ function node(x=false, y=false, r=false, text=""){
         this.text = text;
         this.active = false;
 
-        node.objs[this.id] = this;
+        Graph.objs[this.id] = this;
         this.build();
 
-        if(this.text in node.functions){
-          this.func = node.functions[this.text];
+        if(this.text in Graph.functions){
+          this.func = Graph.functions[this.text];
           this.role = "func";
         }
 
@@ -85,14 +104,14 @@ function node(x=false, y=false, r=false, text=""){
     
     this.build = function(){
       if(this.active){
-        node.ctx.lineWidth = 10;
+        Graph.ctx.lineWidth = 10;
       }else{
-        node.ctx.linewidth = 1;
+        Graph.ctx.linewidth = 1;
       }
-      node.ctx.strokeStyle = this.color;
+      Graph.ctx.strokeStyle = this.color;
       
-      if(node.active == this){
-        node.ctx.strokeStyle = "#aaa";
+      if(Graph.active == this){
+        Graph.ctx.strokeStyle = "#aaa";
       }
       if(this.role == "number"){
         this.text_color = '#0000FF';
@@ -100,14 +119,14 @@ function node(x=false, y=false, r=false, text=""){
       if(this.func !== null){
         this.text_color = "#FF0000";
       }
-    	node.ctx.textAlign = "center";
-      node.ctx.font="15px Arial"; 
-    	node.ctx.beginPath();
-		  node.ctx.arc(this.x,this.y,this.r,0, 2*Math.PI);
-      node.ctx.closePath();
-		  node.ctx.stroke();
-		  node.ctx.fillStyle = this.text_color;
-      node.ctx.fillText(this.text, this.x,this.y);
+    	Graph.ctx.textAlign = "center";
+      Graph.ctx.font="15px Arial"; 
+    	Graph.ctx.beginPath();
+		  Graph.ctx.arc(this.x,this.y,this.r,0, 2*Math.PI);
+      Graph.ctx.closePath();
+		  Graph.ctx.stroke();
+		  Graph.ctx.fillStyle = this.text_color;
+      Graph.ctx.fillText(this.text, this.x,this.y);
         
    
       
@@ -122,11 +141,11 @@ function node(x=false, y=false, r=false, text=""){
       }
       this.children.push(n.id);
       let id_time = new Date().getTime()
-      let id = Number(id_time.toString()+(node.edges.length).toString()) //inline concatination
-      node.edges.push({"id":id, start:this.id, end:n.id, color:"#000", weight:w})
+      let id = Number(id_time.toString()+(Graph.edges.length).toString()) //inline concatination
+      Graph.edges.push({"id":id, start:this.id, end:n.id, color:"#000", weight:w})
 
       n.kill_root();
-      node.drawEdges();
+      Graph.drawEdges();
     }
     this.kill_root = function(){
       this.root = false;
@@ -138,34 +157,34 @@ function node(x=false, y=false, r=false, text=""){
       let angle = Math.atan2(y2-this.ystart,x2-this.xstart);
 
       if(weight == undefined){
-        node.ctx.beginPath();
-        node.ctx.moveTo(this.xstart, this.ystart);
-        node.ctx.lineTo(x2, y2);
-        node.ctx.lineTo(x2-headlen*Math.cos(angle-Math.PI/6),y2-headlen*Math.sin(angle-Math.PI/6));
-        node.ctx.moveTo(x2, y2);
-        node.ctx.lineTo(x2-headlen*Math.cos(angle+Math.PI/6),y2-headlen*Math.sin(angle+Math.PI/6));
-        node.ctx.closePath();
-        node.ctx.stroke();
+        Graph.ctx.beginPath();
+        Graph.ctx.moveTo(this.xstart, this.ystart);
+        Graph.ctx.lineTo(x2, y2);
+        Graph.ctx.lineTo(x2-headlen*Math.cos(angle-Math.PI/6),y2-headlen*Math.sin(angle-Math.PI/6));
+        Graph.ctx.moveTo(x2, y2);
+        Graph.ctx.lineTo(x2-headlen*Math.cos(angle+Math.PI/6),y2-headlen*Math.sin(angle+Math.PI/6));
+        Graph.ctx.closePath();
+        Graph.ctx.stroke();
       }
       else{
         let mid_point = {x:(this.xstart+x2)/2, y:(this.ystart+y2)/2};
         let slope = (y2-this.ystart)/(x2-this.xstart);
-        node.ctx.beginPath();
-        node.ctx.moveTo(this.xstart, this.ystart);
+        Graph.ctx.beginPath();
+        Graph.ctx.moveTo(this.xstart, this.ystart);
         
-        node.ctx.lineTo(mid_point.x, mid_point.y);
+        Graph.ctx.lineTo(mid_point.x, mid_point.y);
         
 
-        node.ctx.moveTo(mid_point.x, mid_point.y);
-        node.ctx.lineTo(x2, y2);
-        node.ctx.lineTo(x2-headlen*Math.cos(angle-Math.PI/6),y2-headlen*Math.sin(angle-Math.PI/6));
-        node.ctx.moveTo(x2, y2);
-        node.ctx.lineTo(x2-headlen*Math.cos(angle+Math.PI/6),y2-headlen*Math.sin(angle+Math.PI/6));
+        Graph.ctx.moveTo(mid_point.x, mid_point.y);
+        Graph.ctx.lineTo(x2, y2);
+        Graph.ctx.lineTo(x2-headlen*Math.cos(angle-Math.PI/6),y2-headlen*Math.sin(angle-Math.PI/6));
+        Graph.ctx.moveTo(x2, y2);
+        Graph.ctx.lineTo(x2-headlen*Math.cos(angle+Math.PI/6),y2-headlen*Math.sin(angle+Math.PI/6));
 
-        node.ctx.closePath();
-        node.ctx.stroke();
-        node.ctx.clearRect(mid_point.x-5, mid_point.y-5, 15, 15);
-        node.ctx.fillText(weight, mid_point.x, mid_point.y+5);
+        Graph.ctx.closePath();
+        Graph.ctx.stroke();
+        Graph.ctx.clearRect(mid_point.x-5, mid_point.y-5, 15, 15);
+        Graph.ctx.fillText(weight, mid_point.x, mid_point.y+5);
 
       }
     }
@@ -205,7 +224,7 @@ function node(x=false, y=false, r=false, text=""){
       
       if(this.value){
         for(let i = 0; i<this.children.length; i++){
-          let child = node.getChild(this.children[i])
+          let child = Graph.getChild(this.children[i])
           child.feed(this.value);
           
         }
@@ -221,7 +240,7 @@ function node(x=false, y=false, r=false, text=""){
         this.value = this.func.apply(this.value, this.args);
         
         for(let i=0; i<this.children.length; i++){
-          let child = node.getChild(this.children[i])
+          let child = Graph.getChild(this.children[i])
           child.feed(this.value);
         }
         this.args = [];
@@ -252,72 +271,53 @@ function node(x=false, y=false, r=false, text=""){
 
 }
 
-node.init = function(canvasid, fps, mouse_move, editable, buildable){
-      node.canvas = document.getElementById(canvasid);
-      node.ctx = node.canvas.getContext('2d');
-      node.objs = {};
-      node.edges = [];
-      if(fps == null){
-        fps = 60;
-      }
-      if(mouse_move == null||true){
-        node.check_mouse();
-      }
-      if(editable == null||true){
-        node.activate_editing(fps);
-      }
-      if(buildable == null||true){
-        node.activate_building();
-      }
-      return node;
 
-}
     
-node.update = function(){
-    node.ctx.clearRect(0,0,node.canvas.width,node.canvas.height);
-    nodes = Object.values(node.objs);
+Graph.update = function(){
+    Graph.ctx.clearRect(0,0,Graph.canvas.width,Graph.canvas.height);
+    nodes = Object.values(Graph.objs);
     nodes.map(node => node.update());
-    node.drawEdges();
-    if(node.connecting){
+    Graph.drawEdges();
+    if(Graph.connecting){
       yflipper = 1;
       xflipper = 1;
-      let slope = (node.mousey-node.start.active.y)/
-      (node.mousex-node.start.active.x);
-      if(node.mousex < node.start.active.x){
+      let slope = (Graph.mousey-Graph.start.active.y)/
+      (Graph.mousex-Graph.start.active.x);
+      if(Graph.mousex < Graph.start.active.x){
         xflipper = -1;
         yflipper = -1;
         
       }
-      node.start.start_node.update_arrow_start_values(slope, xflipper, yflipper);
+      Graph.start.start_node.update_arrow_start_values(slope, xflipper, yflipper);
 
-      node.start.start_node.arrow(node.mousex, node.mousey);
+      Graph.start.start_node.arrow(Graph.mousex, Graph.mousey);
 
-      node.ctx.strokeStyle = node.start.start_node.color;
+      Graph.ctx.strokeStyle = Graph.start.start_node.color;
     }
-    if(node.building){
-      node.ctx.strokeStyle = "#000";
-      node.ctx.beginPath();
-      let x_dist = Math.pow((node.mousex-node.building_start[0]),2);
-      let y_dist = Math.pow((node.mousey-node.building_start[1]),2);
+    if(Graph.building){
+      Graph.ctx.strokeStyle = "#000";
+      Graph.ctx.beginPath();
+      let x_dist = Math.pow((Graph.mousex-Graph.building_start[0]),2);
+      let y_dist = Math.pow((Graph.mousey-Graph.building_start[1]),2);
       let dist = Math.sqrt(x_dist+y_dist);
-      node.ctx.arc(
-        node.building_start[0],
-        node.building_start[1],
+      Graph.ctx.arc(
+        Graph.building_start[0],
+        Graph.building_start[1],
         dist,0,2*Math.PI);
-      node.ctx.closePath();
-      node.ctx.stroke();
+      Graph.ctx.closePath();
+      Graph.ctx.stroke();
     }
 
     
 }
 
-node.activate_editing = function(fps){
+Graph.activate_editing = function(fps){
   
-  var dragging = false;
-  var xdist = null;
-  var ydist = null;
-  var main_node = null;
-  if(node.mouse_activated!==true){
+  let dragging = false;
+  let xdist = null;
+  let ydist = null;
+  let main_node = null;
+  if(Graph.mouse_activated!==true){
     throw "mouse must be first initilized";
   }
   
@@ -325,12 +325,12 @@ node.activate_editing = function(fps){
     if(e.which == 2 || e.which==3){
       return "";
     }
-    if(node.active){  
-      xdist = node.active.x-node.mousex;
-      ydist = node.active.y-node.mousey;
+    if(Graph.active){  
+      xdist = Graph.active.x-Graph.mousex;
+      ydist = Graph.active.y-Graph.mousey;
     
     
-      main_node = node.active;
+      main_node = Graph.active;
       dragging = true;
     }
   }
@@ -345,25 +345,25 @@ node.activate_editing = function(fps){
   }
   function run(){
     if(dragging){
-      main_node.x = node.mousex+xdist;
-      main_node.y = node.mousey+ydist;
-      prevx = node.mousex;
-      prevy = node.mousey;
+      main_node.x = Graph.mousex+xdist;
+      main_node.y = Graph.mousey+ydist;
+      prevx = Graph.mousex;
+      prevy = Graph.mousey;
     }
-    node.update();
+    Graph.update();
     setTimeout(run,1000/fps);
     
   }
   
-  node.canvas.addEventListener("mousedown", start_dragging);
-  node.canvas.addEventListener("mouseup",end_dragging);
+  Graph.canvas.addEventListener("mousedown", start_dragging);
+  Graph.canvas.addEventListener("mouseup",end_dragging);
   run();  
 }
 
-node.drawEdges = function(){
-  for(edge of node.edges){
-    let start = node.getChild(edge.start);
-    let end = node.getChild(edge.end);
+Graph.drawEdges = function(){
+  for(edge of Graph.edges){
+    let start = Graph.getChild(edge.start);
+    let end = Graph.getChild(edge.end);
     start.slope = (start.y-end.y)/(start.x-end.x);
     start.xflipper = 1;
     start.yflipper = 1;
@@ -382,47 +382,47 @@ node.drawEdges = function(){
               
     start.yend = end.y -start.yflipper*(Math.sin(Math.atan(start.slope))*end.r);
 
-    node.ctx.strokeStyle = edge.color;
+    Graph.ctx.strokeStyle = edge.color;
             
     start.arrow(start.xend, start.yend, edge.weight);
     
-    node.ctx.strokeStyle = this.color;
+    Graph.ctx.strokeStyle = this.color;
   }
 }
 
-node.getChild = function(id){
-  return node.objs[id];
+Graph.getChild = function(id){
+  return Graph.objs[id];
 }
 
-node.getEdge = function(parentid, childid){
-  for(edge of node.edges){
+Graph.getEdge = function(parentid, childid){
+  for(edge of Graph.edges){
     if(edge.start === parentid && edge.end === childid){
       return edge;
     }
   }
 }
 
-node.check_mouse = function(){
-  let rect = node.canvas.getBoundingClientRect();
-  node.offsetx = rect.left;
-  node.offsety = rect.top;
-  node.active = null;
-  node.mouse_activated = true;
+Graph.check_mouse = function(){
+  let rect = Graph.canvas.getBoundingClientRect();
+  Graph.offsetx = rect.left;
+  Graph.offsety = rect.top;
+  Graph.active = null;
+  Graph.mouse_activated = true;
   function check(e){
-    node.mousex = e.x-rect.left;
-    node.mousey = e.y-rect.top;
-    let nodes = Object.values(node.objs);
+    Graph.mousex = e.x-rect.left;
+    Graph.mousey = e.y-rect.top;
+    let nodes = Object.values(Graph.objs);
     for(var i =0; i<nodes.length; i++){
-      if(nodes[i].inside(node.mousex,node.mousey,node.px_down)){
+      if(nodes[i].inside(Graph.mousex,Graph.mousey,Graph.px_down)){
         document.body.style.cursor = "pointer";
-        node.active = nodes[i];
+        Graph.active = nodes[i];
     
         return;
         
       }
     }
     document.body.style.cursor = "default";
-    node.active = null;
+    Graph.active = null;
     
     
   }
@@ -432,26 +432,26 @@ node.check_mouse = function(){
     
     let scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
     
-    node.px_down = scrollTop;
+    Graph.px_down = scrollTop;
     
   }
-  node.yscroll = 0;
-  node.px_down = 0;
-  node.canvas.addEventListener("mousemove", check);
+  Graph.yscroll = 0;
+  Graph.px_down = 0;
+  Graph.canvas.addEventListener("mousemove", check);
   document.addEventListener("scroll",adjust_scoll);
 }
 
-node.activate_building = function(){
+Graph.activate_building = function(){
   
-  node.canvas.addEventListener("contextmenu",
+  Graph.canvas.addEventListener("contextmenu",
   function(e){e.preventDefault()});
-  node.building = false;
-  node.canvas.addEventListener("mousedown", right_click);
+  Graph.building = false;
+  Graph.canvas.addEventListener("mousedown", right_click);
   document.addEventListener("keydown", capture);
-  node.canvas.addEventListener("mouseup", release_click);
+  Graph.canvas.addEventListener("mouseup", release_click);
   //node.canvas.addEventListener("dbclick", gui_build);
-  node.start = {"x":null, "y":null};
-  var action = null;
+  Graph.start = {"x":null, "y":null};
+  let action = null;
   
   
   function gui_build(e){
@@ -462,21 +462,21 @@ node.activate_building = function(){
       return "";
     }
     if(e.which == 1){
-      if(node.building){
-        let x_dist = Math.pow((node.mousex-node.building_start[0]),2);
-        let y_dist = Math.pow((node.mousey-node.building_start[1]),2);
+      if(Graph.building){
+        let x_dist = Math.pow((Graph.mousex-Graph.building_start[0]),2);
+        let y_dist = Math.pow((Graph.mousey-Graph.building_start[1]),2);
         let dist = Math.sqrt(x_dist+y_dist);
-        new node().create(node.building_start[0],node.building_start[1],
+        new Graph.node().create(Graph.building_start[0],Graph.building_start[1],
         dist, "");
-        node.building = false;
+        Graph.building = false;
       }
     }
     if(e.which == 3){
-      if(node.active != null){
-        node.start["start_node"].connect(node.active, 10);
+      if(Graph.active != null){
+        Graph.start["start_node"].connect(Graph.active, 10);
       }
-      node.connecting = false;
-      node.start = {"x":null, "y":null};
+      Graph.connecting = false;
+      Graph.start = {"x":null, "y":null};
     }
   }
   function right_click(e){
@@ -485,26 +485,26 @@ node.activate_building = function(){
       return "";
     }
     if(e.which ==1){
-      if(node.active == null){
-        node.building = true;
-        node.building_start = [node.mousex,node.mousey];
+      if(Graph.active == null){
+        Graph.building = true;
+        Graph.building_start = [Graph.mousex,Graph.mousey];
         
       }
     }
     if(e.which == 3){
-      node.start = {"x":null, "y":null};
+      Graph.start = {"x":null, "y":null};
       
-      node.start = {"x":node.mousex,"y":node.mousey, "active":node.active};
+      Graph.start = {"x":Graph.mousex,"y":Graph.mousey, "active":Graph.active};
       
-      if(node.active != null){
+      if(Graph.active != null){
         
-        node.connecting = false;
-        node.start["action"] = "connect";
-        node.start["start_node"] = node.active;
-        node.connecting = true;
-        node.start["start_node"].connect(node.active);
+        Graph.connecting = false;
+        Graph.start["action"] = "connect";
+        Graph.start["start_node"] = Graph.active;
+        Graph.connecting = true;
+        Graph.start["start_node"].connect(Graph.active);
       }else{
-        node.connecting = false;
+        Graph.connecting = false;
       }
     }
     
@@ -512,16 +512,16 @@ node.activate_building = function(){
   
   function capture(e){
     letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+~\\/?<>'\".,;:~`[]{}|-= ";
-    if(node.active != null){
+    if(Graph.active != null){
       if(letters.indexOf(e.key)>-1){
         e.output =  e.key;
       }
       
       if(e.key == "Enter"){
-        node.active.feed();
+        Graph.active.feed();
       }
       if(e.key == "Delete"){
-        node.active.delete();
+        Graph.active.delete();
 
         
       }
@@ -529,34 +529,34 @@ node.activate_building = function(){
         e.preventDefault();
       }
       if(e.key == "Backspace"){
-        node.active.text = node.active.text.substring(0,node.active.text.length-1);
+        Graph.active.text = Graph.active.text.substring(0,Graph.active.text.length-1);
         e.output = "";
       }
       
 
       
       if(e.output){
-        node.active.text += e.output;
-        if(node.active.text in node.functions){
-          node.active.func = node.functions[node.active.text];
-          node.active.role = "func";
+        Graph.active.text += e.output;
+        if(Graph.active.text in Graph.functions){
+          Graph.active.func = Graph.functions[Graph.active.text];
+          Graph.active.role = "func";
         }
-        else if(node.active.text.charAt(0) == "*"){
-          node.active.role = 'function';
+        else if(Graph.active.text.charAt(0) == "*"){
+          Graph.active.role = 'function';
           
         }
-        else if(parseFloat(node.active.text)){
-          node.active.set_value(parseFloat(node.active.text));
-          node.active.role = 'number';
+        else if(parseFloat(Graph.active.text)){
+          Graph.active.set_value(parseFloat(Graph.active.text));
+          Graph.active.role = 'number';
         }else{
           
-          node.active.role = 'string';
+          Graph.active.role = 'string';
         }
         
 
         
       }
-      node.capture_callback(node.active, e.key);
+      Graph.capture_callback(Graph.active, e.key);
       
 
     }
@@ -564,7 +564,7 @@ node.activate_building = function(){
   }
 
 }
-node.functions = {
+Graph.functions = {
   "end":()=>{}
 
                   }
